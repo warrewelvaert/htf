0b4eb5a6ea36cca3011ff9a73ad3fc30a5c6c94b
sam.rafie <sam.rafie@student.howest.be>
2023-11-09 11:22:18 +0100

    can log

--- src/main/java/be/thebeehive/htf/client/Main.java
+++ src/main/java/be/thebeehive/htf/client/Main.java
@@ -2,15 +2,17 @@ package be.thebeehive.htf.client;
 
 import be.thebeehive.htf.library.EnvironmentType;
 import be.thebeehive.htf.library.HtfClient;
+import be.thebeehive.htf.library.protocol.client.RemoveDisruptorsClientMessage;
 
 import java.net.URISyntaxException;
+import java.rmi.server.UID;
 
 public class Main {
 
     public static void main(String[] args) throws URISyntaxException {
         HtfClient client = new HtfClient(
                 "wss://htf.b9s.dev/ws",
-                "",
+                "Felicitas5507",
                 EnvironmentType.SIMULATION,
                 new MyClient()
         );
--- src/main/java/be/thebeehive/htf/client/MyClient.java
+++ src/main/java/be/thebeehive/htf/client/MyClient.java
@@ -8,10 +8,16 @@ import be.thebeehive.htf.library.protocol.server.GameEndedServerMessage;
 import be.thebeehive.htf.library.protocol.server.GameRoundServerMessage;
 import be.thebeehive.htf.library.protocol.server.WarningServerMessage;
 
+import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class MyClient implements HtfClientListener {
-
+    private static final Logger LOGGER = Logger.getLogger(MyClient.class.getName());
     /**
      * You tried to perform an action that is not allowed.
      * An error occurred, and we are unable to recover from this.
@@ -19,7 +25,7 @@ public class MyClient implements HtfClientListener {
      */
     @Override
     public void onErrorServerMessage(HtfClient client, ErrorServerMessage msg) throws Exception {
-
+        LOGGER.log(Level.SEVERE, msg.getMsg());
     }
 
     /**
@@ -27,7 +33,7 @@ public class MyClient implements HtfClientListener {
      */
     @Override
     public void onGameEndedServerMessage(HtfClient client, GameEndedServerMessage msg) throws Exception {
-
+        LOGGER.log(Level.INFO, "Game ended!");
     }
 
     /**
@@ -36,8 +42,28 @@ public class MyClient implements HtfClientListener {
      */
     @Override
     public void onGameRoundServerMessage(HtfClient client, GameRoundServerMessage msg) throws Exception {
-        client.send(new RemoveDisruptorsClientMessage(msg.getRoundId(), Collections.emptyList()));
+        LOGGER.info("Disruptor this round");
+        logDisruptors(msg.getDisruptors(), msg.getRoundId());
+
+        List<GameRoundServerMessage.JungleDisruptor> disruptors = msg.getDisruptors();
+
+        Collections.sort(disruptors, (d1, d2) -> d2.getStats().get(0).getInitialDamage().compareTo(d1.getStats().get(0).getInitialDamage()));
+
+        // For example, you might decide to remove the disruptors with the highest initial damage.
+        List<Long> disruptorsToRemove = disruptors.stream()
+                .limit(msg.getDisruptorsToRemove())
+                .map(GameRoundServerMessage.JungleDisruptor::getId)
+                .collect(Collectors.toList());
+
+        LOGGER.info("Disruptor to remove");
+        logDisruptors(disruptors.stream()
+                .filter(disruptor -> disruptorsToRemove.contains(disruptor.getId()))
+                .collect(Collectors.toList()), msg.getRoundId());
+
+        // Send a RemoveDisruptorsClientMessage with the IDs of the disruptors to remove.
+        client.send(new RemoveDisruptorsClientMessage(msg.getRoundId(), disruptorsToRemove));
     }
+
 
     /**
      * You tried to perform an action that is not allowed.
@@ -46,6 +72,38 @@ public class MyClient implements HtfClientListener {
      */
     @Override
     public void onWarningServerMessage(HtfClient client, WarningServerMessage msg) throws Exception {
-
+        LOGGER.log(Level.INFO, "Invalid move");
     }
+
+    private void logDisruptors(List<GameRoundServerMessage.JungleDisruptor> dis, UUID roundId) {
+
+        for (GameRoundServerMessage.JungleDisruptor disruptor : dis) {
+            StringBuilder disruptorLog = new StringBuilder();
+            disruptorLog.append("Disruptor Details:\n")
+                    .append("ID: ").append(disruptor.getId()).append("\n")
+                    .append("Init rounds: ").append(disruptor.getInitialRound()).append("\n")
+                    .append("Max rounds: ").append(disruptor.getMaxRounds()).append("\n")
+                    .append("Activation chance: ").append(disruptor.getActivationChance()).append("\n");
+
+            List<GameRoundServerMessage.JungleDisruptorStat> stats = disruptor.getStats();
+            for (GameRoundServerMessage.JungleDisruptorStat stat : stats) {
+                disruptorLog.append("Init damage: ").append(stat.getInitialDamage()).append("\n")
+                        .append("Round multiplier: ").append(stat.getRoundMultiplier()).append("\n");
+            }
+            LOGGER.info(disruptorLog.toString());
+        }
+    }
+
+    private BigDecimal calculateTotalDamage(GameRoundServerMessage.JungleDisruptor disruptor, int rounds) {
+        BigDecimal totalDamage = BigDecimal.ZERO;
+        BigDecimal damage = disruptor.getStats().get(0).getInitialDamage();
+
+        for (int i = 0; i < rounds; i++) {
+            totalDamage = totalDamage.add(damage);
+            damage = damage.multiply(disruptor.getStats().get(0).getRoundMultiplier());
+        }
+
+        return totalDamage;
+    }
+
 }
